e59c55d706b146a851ecdc4c36bbe60d07a3df0e
Jesse Theisen <theiseja@gmail.com>
2019-01-23 18:29:59 -0600

    MVP: Finishing up actions now

--- actions/actionRoutes.js
+++ actions/actionRoutes.js
@@ -3,5 +3,32 @@ const actionDB = require('../data/helpers/actionModel')
 const projectDB = require('../data/helpers/projectModel')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    actionDB
+    .get()
+    .then(actions => {
+        res.json(actions)
+    })
+    .catch(() => {
+        res.status(500).json({ error: 'The actions could not be retrived from the server.'})
+    })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    actionDB
+    .get(id)
+    .then(actions => {
+        if (actions) {
+            res.json(actions)
+        } else {
+            res.status(404).json({ message: 'The action with that ID does not exist within the DB.'})
+        }
+    })
+    .catch(() => {
+        res.status(404).json({ error: 'Information aobut the action could not be retrieved from the server.'})
+    })
+})
+
 
 module.exports = router
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const projectRouters = require('./projects/projectRoutes');
 const actionRoutes = require('./actions/actionRoutes');
 const mwConfig = require('./data/mwConfig');
 
-const PORT = 5000;
+const PORT = 4000;
 const server = express();
 
 mwConfig(server);
@@ -11,4 +11,4 @@ mwConfig(server);
 server.use('./api/projects', projectRouters);
 server.use('./api/actions', actionRoutes);
 
-server.listen(PORT, () => console.log(`API running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
