5a003c8a718d3d4603d57fd379a93a0665600770
Jesse Theisen <theiseja@gmail.com>
2019-03-09 11:57:42 -0600

    Redo: Projects completed and tested

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -4,6 +4,8 @@ const actionDB = require("../data/helpers/actionModel");
 const router = express.Router();
 
 // endpoints
+
+//confirmed working
 router.get("/", (req, res) => {
   actionDB
     .get()
@@ -16,6 +18,60 @@ router.get("/", (req, res) => {
         .json({ error: "The actions could not be retrived from the database." });
     });
 });
+
+//confirmed working after testing
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    actionDB
+      .get(id)
+      .then(actions => {
+        if (actions) {
+          res.json(actions);
+        } else {
+          res.status(404).json({
+            message: "The action with that ID does not exist within the DB."
+          });
+        }
+      })
+      .catch(() => {
+        res.status(404).json({
+          error:
+            "Information aobut the action could not be retrieved from the server."
+        });
+      });
+  });
+
+  router.post("/", (req, res) => {
+    const action = req.body;
+    if (action.project_id && action.description && action.notes) {
+      projectDB
+        .get(action.project_id)
+        .then(projects => {
+          if (projects) {
+            actionDB
+              .insert(action)
+              .then(action => {
+                res.status(201).json(action);
+              })
+              .catch(() => {
+                res.status(500).json({
+                  error: "There was an error while saving action to the database."
+                });
+              });
+          } else {
+            res.status(404).json({ message: "This project does not exist." });
+          }
+        })
+        .catch(() => {
+          res.status(404).json({
+            error:
+              "Project using that ID does not exist within the DB, please enter one that is contained within the DB."
+          });
+        });
+    } else {
+      res.status(400).json({ error: "Please provide more info" });
+    }
+  });
 
 //end of file
 module.exports = router;
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -72,7 +72,64 @@ router.post("/", (req, res) => {
     });
 });
 
+// confirmed working, can't update without name or description
+router.put("/:id", (req, res) => {
+    const project = req.body;
+    const { id } = req.params;
+    if (project.name && project.description) {
+      projectDB
+        .update(id, project)
+        .then(count => {
+          if (count) {
+            projectDB.get(id).then(project => {
+              res.json(project);
+            });
+          } else {
+            res.status(404).json({
+              message:
+                "The project with the specified ID does not exist within the DB."
+            });
+          }
+        })
+        .catch(() => {
+          res
+            .status(500)
+            .json({ error: "The project information could not be modifed." });
+        });
+    } else {
+      res.status(400).json({ errorMessage: "Please provide more project info." });
+    }
+  });
 
 
+  //confirmed working. Deletes all actions associated with the project ID so 
+  //no ghost data remains in the database
+  router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    projectDB
+      .remove(id)
+      .then(count => {
+        if (count) {
+          projectDB.getProjectActions(id).then(actions => {
+            actions.map(action => {
+              console.log("action.id", action.id);
+              actionDB.remove(action.id).then(() => {
+                console.log("Action Deleted!");
+              });
+            });
+            res.json({ message: "This project successfully deleted." });
+          });
+        } else {
+          res.status(404).json({
+            message: "The project with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ error: "The project could not be removed from the DB." });
+      });
+  });
 
 module.exports = router;
