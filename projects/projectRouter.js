278fa96d1728f737e104ea2476b19aa99c84984a
Jesse Theisen <theiseja@gmail.com>
2019-03-09 11:41:10 -0600

    Redo: Added GET endpoints for projects

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -3,15 +3,60 @@ const projectDB = require('../data/helpers/projectModel');
 const actionDB = require('../data/helpers/actionModel');
 const router = express.Router();
 
+//confirmed working
+router.get("/", (req, res) => {
+    projectDB
+      .get()
+      .then(projects => {
+        res.json(projects);
+      })
+      .catch(() => {
+        res.status(500).json({ error: "Unable to retrieve projects from the database." });
+      });
+  });
 
-router.get('/', async(req,res) => {
-    try{
-        const projectDB = await projects.get()
-        res.status(200).json(projectsData)
-    }catch(err){
-        res.status(500).json({ err: 'Unable to retrieve the projects from the database.'})
-    }
-});
+  //confirmed working & can't load a project that doesn't exist
+  router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    projectDB
+      .get(id)
+      .then(projects => {
+        console.log("projects", projects);
+        if (projects) {
+          res.json(projects);
+        } else {
+          res.status(404).json({ message: "This project does not exist." });
+        }
+      })
+      .catch(() => {
+        res.status(404).json({
+          error: "Information about the project could not be retrieved."
+        });
+      });
+  });
+
+  //confirmed working, pulls the actions tied to a specific project ID
+  router.get("/:id/actions", (req, res) => {
+    const { id } = req.params;
+    projectDB
+      .getProjectActions(id)
+      .then(actions => {
+        if (actions.length) {
+          res.json(actions);
+        } else {
+          res
+            .status(404)
+            .json({ message: "Action does not exist within the DB." });
+        }
+      })
+      .catch(() => {
+        res
+          .status(404)
+          .json({ error: "Info about this action could not be retrieved." });
+      });
+  });
+
+
 
 
 module.exports = router;
