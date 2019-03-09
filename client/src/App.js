bbe5d66e39986f4e7505d524ba43ebbf1ecf2867
Jesse Theisen <theiseja@gmail.com>
2019-03-09 12:29:19 -0600

    Redo: attempting stretch, have projects rendering at the moment. Saving progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
-import Projects from './components/Projects.js'
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       projects: []
-    }
+    };
   }
 
   componentDidMount() {
-    axios.get("http://localhost:9090/api/projects")
+    axios
+      .get("http://localhost:9090/api/projects")
       .then(res => {
-        this.setState({projects: res.data})
+        this.setState({ projects: res.data });
+        console.log(this.state.projects);
       })
       .catch(err => {
-        console.log(err)
-      })
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div className="App">
-        <h2>Projects</h2>
-        <Projects projects={this.state.projects} />
+        {this.state.projects.map(project => {
+          return (
+            <div key={project.id}>
+              <div>Project ID: {project.id}</div>
+              <div>Description: {project.description}</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
